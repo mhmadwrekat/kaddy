73f52be1ee0db673196a578cdba5b474555d0764
mhmad wrekat <mhmmadwreekkat@gmail.com>
2022-04-23 16:29:25 +0300

    update

--- pages/index.js
+++ pages/index.js
@@ -18,33 +18,33 @@ const index = () => {
 <>
 <section className=''>
 <Nav />
-<br></br><br></br><br></br>
+<br></br>
 <div className="text-center">
 
 
-
-
-<Slide cascade>
-<Slide>
+<Slide cascade triggerOnce>
+<Slide triggerOnce>
 <p className="mx-5 font-sans text-5xl font-bold text-teal-700">Kaddy Oil</p>
 </Slide>
 <br></br>
-<br></br>
-<Slide>
+<Slide triggerOnce>
 <p className="mx-5 font-sans text-xl font-bold text-teal-700">
 زيت كادي هو مجموعة من الزيوت والفيتامينات الطبيعية للعناية بالاظافر بالطريقة الصحيحة
 </p>
 </Slide>
 <br></br>
-<Slide>
+<Slide triggerOnce>
 <p className="mx-5 font-sans text-xl font-bold text-teal-700">Kaddy Oil is a group of natural oils and vitamins to take care of nails in the right way</p>
 </Slide>
-
+<br></br>
+<Slide triggerOnce>
+<img className="w-4/6 mx-auto rounded-full lg:w-3/12"
+src='./assest/logo2.jpg'></img>
 </Slide>
-<Reveal delay={3000} keyframes={customAnimation}>
+</Slide>
+<br></br>
+<Reveal delay={3000} triggerOnce keyframes={customAnimation}>
 <div>
-<br></br><br></br><br></br><br></br>
-<br></br><br></br><br></br>
 <button class="
 font-mono bg-teal-600 hover:bg-teal-700 text-white font-bold py-8 px-20 border-b-4 border-teal-800 hover:border-teal-900 rounded-xl text-3xl">
  !! إبدأ
@@ -53,6 +53,7 @@ font-mono bg-teal-600 hover:bg-teal-700 text-white font-bold py-8 px-20 border-b
 </Reveal>
 
 
+<br></br>
 
 </div>
 </section>
