f506f79673143f5a68157d4dd002813478770a9d
mhmad wrekat <mhmmadwreekkat@gmail.com>
2022-04-23 15:17:10 +0300

    update

--- pages/index.js
+++ pages/index.js
@@ -4,13 +4,13 @@ import Nav from '../components/Nav';
 const index = () => {
   return (
 <>
-<section className='h-screen bg-green-700'>
+<section className='h-screen bg-indigo-800'>
 <Nav />
 <br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br>
 <br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br>
 <div className='text-center'>
 <button class="
-font-mono bg-amber-500 hover:bg-amber-600 text-white font-bold py-10 px-20 border-b-4 border-amber-600 hover:border-amber-600 rounded-xl text-3xl">
+font-mono bg-amber-400 hover:bg-amber-500 text-white font-bold py-10 px-20 border-b-4 border-amber-600 hover:border-amber-700 rounded-xl text-3xl">
  !! إبدأ
 </button>
 </div>
