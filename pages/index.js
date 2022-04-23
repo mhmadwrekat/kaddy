58c6dff6a1fa185d4f3a6de5e6a9c36a9b549292
mhmad wrekat <mhmmadwreekkat@gmail.com>
2022-04-23 15:09:56 +0300

    update

--- pages/index.js
+++ pages/index.js
@@ -6,27 +6,14 @@ const index = () => {
 <>
 <section className='h-screen bg-purple-800'>
 <Nav />
-<br></br>
-<br></br>
-<br></br>
-<br></br>
-<br></br>
-<br></br>
-<br></br>
-
+<br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br>
+<br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br>
 <div className='text-center'>
-<button class="font-mono bg-amber-500 hover:bg-amber-600 text-white font-bold py-10 px-20 border-b-4 border-amber-600 hover:border-amber-600 rounded-full text-3xl">
+<button class="
+font-mono bg-amber-500 hover:bg-amber-600 text-white font-bold py-10 px-20 border-b-4 border-amber-600 hover:border-amber-600 rounded-xl text-3xl">
  !! إبدأ
 </button>
 </div>
-
-<br></br>
-<br></br>
-<br></br>
-<br></br>
-<br></br>
-<br></br>
-<br></br>
 </section>
 </>  
 )
