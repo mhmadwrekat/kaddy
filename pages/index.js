78bdd065d504dc48faaf6369efd6e8aed7b39556
mhmad wrekat <mhmmadwreekkat@gmail.com>
2022-04-23 15:55:14 +0300

    update

--- pages/index.js
+++ pages/index.js
@@ -25,16 +25,17 @@ const index = () => {
 
 
 <Fade cascade>
-<p className="font-sans text-5xl font-bold text-gray-200">Kaddy Oil</p>
+<p className="mx-5 font-sans text-5xl font-bold text-gray-200">Kaddy Oil</p>
 <br></br>
-<p className="font-sans text-xl font-bold text-gray-200">
+<br></br>
+<p className="mx-5 font-sans text-xl font-bold text-gray-200">
 زيت كادي هو مجموعة من الزيوت والفيتامينات الطبيعية للعناية بالاظافر بالطريقة الصحيحة
 </p>
 <br></br>
-<p className="font-sans text-xl font-bold text-gray-200">Kaddy Oil is a group of natural oils and vitamins to take care of nails in the right way</p>
+<p className="mx-5 font-sans text-xl font-bold text-gray-200">Kaddy Oil is a group of natural oils and vitamins to take care of nails in the right way</p>
 <div>
 <br></br><br></br><br></br><br></br>
-<br></br><br></br><br></br><br></br>
+<br></br><br></br><br></br>
 <Reveal keyframes={customAnimation}>
 <button class="
 font-mono bg-amber-500 hover:bg-amber-600 text-white font-bold py-8 px-20 border-b-4 border-amber-600 hover:border-amber-600 rounded-xl text-3xl">
