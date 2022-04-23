cdce6f69784b72ca383553c5ea34d5929a7bc8ee
mhmad wrekat <mhmmadwreekkat@gmail.com>
2022-04-23 22:52:58 +0300

    update

--- pages/index.js
+++ pages/index.js
@@ -33,14 +33,13 @@ src='./assest/logo2.jpg'></img>
 </Fade>
 <Slide>
 <div>
-
+<Link href="/home">
 <button translate='no'
 class="animate-bounce
 font-mono bg-teal-600 hover:bg-teal-600 text-white font-bold py-5 px-20 border-b-4 border-teal-800 hover:border-teal-900 rounded-xl shadow-2xl text-3xl">
-<Link href="/home">
-Start!! </Link>
+Start!
 </button>
-
+</Link>
 </div>
 </Slide>
 <br></br>
