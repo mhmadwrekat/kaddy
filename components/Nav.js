afb87ab8c8f40f78b16a3e0c4586b3361d75e7f0
mhmad wrekat <mhmmadwreekkat@gmail.com>
2022-04-23 14:56:45 +0300

    update

---
+++ components/Nav.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Head from 'next/head';
+const Nav = () => {
+  return (
+    <Head>
+            <title>Kaddy Oil</title>
+            <link rel="icon" href="/assest/logo.jpg" />
+            <meta charset="UTF-8" />
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+            <meta name="description" content="Kaddy Natural Oils  - Jordan-Amman" />
+            <link rel="stylesheet" href="https://unpkg.com/tailwindcss@2.2.19/dist/tailwind.min.css" />
+            <link href="https://fonts.googleapis.com/css?family=Work+Sans:200,400&display=swap" rel="stylesheet" />
+            <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/tw-elements/dist/css/index.min.css" />
+    </Head>
+    )
+}
+export default Nav
---
+++ pages/index.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import Home from './Home';
+import Nav from '../components/Nav';
+const index = () => {
+  return (
+<>
+<section className='h-screen bg-purple-800'>
+<Nav />
+<br></br>
+<br></br>
+<br></br>
+<br></br>
+<br></br>
+<br></br>
+<br></br>
+
+<div className='text-center'>
+<button class="font-mono bg-amber-500 hover:bg-amber-600 text-white font-bold py-10 px-20 border-b-4 border-amber-600 hover:border-amber-600 rounded-full text-3xl">
+ !! إبدأ
+</button>
+</div>
+
+<br></br>
+<br></br>
+<br></br>
+<br></br>
+<br></br>
+<br></br>
+<br></br>
+</section>
+</>  
+)
+}
+
+export default index;
