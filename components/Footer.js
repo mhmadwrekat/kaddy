6f63c6b28e8841fb07102230d01f6d57926281d7
mhmad wrekat <mhmmadwreekkat@gmail.com>
2022-04-25 05:53:24 +0300

    add footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,14 +1,38 @@
-import React from 'react'
+import React, { useRef, useState, useEffect } from "react";
+import swal from 'sweetalert';
+import emailjs from '@emailjs/browser';
+const SERVICE_ID = process.env.NEXT_PUBLIC_EMAIL_SERVICE_ID;
+const USER_ID = process.env.NEXT_PUBLIC_EMAIL_USER_ID;
+const TEMPLATE_ID = process.env.NEXT_PUBLIC_EMAIL_TEMPLATE_ID;
 
 const Footer = () => {
-  return (
+  const form = useRef();
+
+  const handelSubscribe = (event) => {
+  event.preventDefault();
+  const subNumber = event.target.number.value ;
+  console.log("NNNNN -> ",subNumber);
+  swal("!تم بنجاح" , "سوف يتم التواصل معك من خلال واتس اب", "success");
+  emailjs.sendForm(`${SERVICE_ID}`, `${TEMPLATE_ID}`, form.current, `${USER_ID}`);
+  event.target.reset();
+}
+
+
+
+
+
+
+
+
+
+
+return (
 <>
 
 
 <footer class="text-center bg-white text-teal-600 bg-gradient-to-r from-white to-teal-100 ...">
   <div class="container px-6">
     <div class="flex justify-center gap-x-12 py-6">
-
 <a href="https://wa.me/+962788818125" data-tippy-content="WhatsApp">
 <img src='./assest/whatsapp.png'
 className='w-6 h-6 fill-current lg:w-7 lg:h-7 hover:scale-125 cursor-alias' />
@@ -16,12 +40,12 @@ className='w-6 h-6 fill-current lg:w-7 lg:h-7 hover:scale-125 cursor-alias' />
 <a className="pb-0.5 rounded-full hover:scale-125" href="https://www.facebook.com/Kaddy-oil-919674034765987"
 data-tippy-content="Facebook">
 <img src='./assest/facebook.png'
-className='w-6 h-6 lg:w-7 lg:h-7 fill-curren cursor-alias' />
+className='w-6 h-6 mt-0 lg:w-7 lg:h-7 fill-curren cursor-alias' />
 </a>
 <a className="rounded-full hover:scale-125" href="https://www.instagram.com/kaddy_oil/?utm_source=qr"
 data-tippy-content="Instagram">
 <img src='./assest/instagram.png'
-className='w-6 h-6 fill-current cursor-alias lg:w-7 lg:h-7'/></a>
+className='w-6 h-6 mt-0.5 fill-current cursor-alias lg:w-7 lg:h-7'/></a>
     </div>
 
 
@@ -29,20 +53,23 @@ className='w-6 h-6 fill-current cursor-alias lg:w-7 lg:h-7'/></a>
 
 {/*
   MOBILE FORM
+
 */}
     <div className='block md:hidden lg:hidden'>
-      <form action="">
+      <form onSubmit={handelSubscribe} >
         <div class="grid md:grid-cols-3 gird-cols-1 gap-4 justify-center items-center">
           <div class="md:ml-auto md:mb-6">
             <p class="">
               <strong>
-              ! أشترك معنا لتصلك عروضنا
+              ! أدخل رقمك لتصلك عروضنا
               </strong>
             </p> 
           </div>
           <div class="md:mb-6">
             <input
               type="number"
+              name="number"
+              min="0770000000" step="1" max="0799999999"
               class="
                 form-control
                 block
@@ -60,7 +87,8 @@ className='w-6 h-6 fill-current cursor-alias lg:w-7 lg:h-7'/></a>
                 m-0
                 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
               id="exampleFormControlInput1"
-              placeholder="07XX XXX XXX"/>
+              placeholder="07XX XXX XXX"
+              required />
           </div>
           <div class="md:mr-auto mb-6">
           <button type="submit" class="inline-block px-6 py-2 border-2 bg-teal-700 border-teal-500 text-white font-medium text-xs leading-tight uppercase rounded hover:bg-white hover:text-teal-700 hover:bg-opacity-5 focus:outline-none focus:ring-0 transition duration-150 ease-in-out">
@@ -74,17 +102,18 @@ className='w-6 h-6 fill-current cursor-alias lg:w-7 lg:h-7'/></a>
   DESKTOP FORM
 */}
 <div className='hidden md:block lg:block'>
-      <form action="">
+      <form onSubmit={handelSubscribe}>
         <div class="grid md:grid-cols-3 gird-cols-1 gap-4 justify-center items-center">
           <div class="md:ml-auto md:mb-6">
           <button type="submit" class="inline-block px-6 py-2 border-2 bg-teal-700 border-teal-500 text-white font-medium text-xs leading-tight uppercase rounded hover:bg-white hover:text-teal-700 hover:bg-opacity-5 focus:outline-none focus:ring-0 transition duration-150 ease-in-out">
               إرسال
-            </button>
-             
+            </button>             
           </div>
           <div class="md:mb-6">
             <input
               type="number"
+              name="number"
+              min="0770000000" step="1" max="0799999999"
               class="
                 form-control
                 block
@@ -102,12 +131,13 @@ className='w-6 h-6 fill-current cursor-alias lg:w-7 lg:h-7'/></a>
                 m-0
                 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
               id="exampleFormControlInput1"
-              placeholder="07XX XXX XXX"/>
+              placeholder="07XX XXX XXX"
+              required />
           </div>
           <div class="md:mr-auto mb-6">
           <p class="">
               <strong>
-              ! أشترك معنا لتصلك عروضنا
+              ! أدخل رقمك لتصلك عروضنا
               </strong>
             </p>
           </div>
