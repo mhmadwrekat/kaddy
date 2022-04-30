d089b8bef0ede6bb184b265063d3b3cad4df2a7a
Mhmad Wrekat <86598070+mhmadwrekat@users.noreply.github.com>
2022-04-30 18:24:03 +0300

    Update Main.js

--- components/Main.js
+++ components/Main.js
@@ -1,17 +1,32 @@
 import React from 'react';
 import Header from './Header';
 import Collapse from './Collapse';
-
-
+import Carosal from './Carosal';
+import Dcarosal from './Dcarosal';
+import Footer from './Footer';
 
 
 const Main = () => {
   
 return (
 <>
-<section className='h-screen bg-gradient-to-r from-white to-teal-100 ...'>
+<section className='bg-gradient-to-r from-white to-teal-100 ...'>
 
 <Header />
+
+
+{/* Mobile Carosal */}
+<section className='block md:hidden lg:hidden'>
+<Carosal />
+</section>
+{/* Desktop Carosal */}
+<section className='hidden md:block lg:block'>
+<Dcarosal />
+</section>
+
+
+
+
 
 <div id="Who?">
 <Collapse />
@@ -24,6 +39,7 @@ return (
 
 
 
+<Footer />
 
 </section>
 </>
