1242086955de5305339aecfb91f345e5576b2c05
mhmad wrekat <mhmmadwreekkat@gmail.com>
2022-04-30 19:39:36 +0300

    update carosal

--- components/Carosal.js
+++ components/Carosal.js
@@ -34,21 +34,21 @@ const Carosal = () => {
                 >
 
                     <SwiperSlide>
+                        <img src="./assest/Carosal1.jpg"
+                            className="block w-full shadow-2xl" 
+                      onClick={() => { window.open('./assest/Carosal1.jpg', '_self') }}
+                      />
+                    </SwiperSlide>
+                    <SwiperSlide>
                         <img src="./assest/Carosal2.jpg"
                             className="block w-full shadow-2xl" 
-                      onClick={() => { window.open('./assest/kaddy10.jpg', '_self') }}
+                      onClick={() => { window.open('./assest/Carosal2.jpg', '_self') }}
                       />
                     </SwiperSlide>
                     <SwiperSlide>
                         <img src="./assest/Carosal3.jpg"
                             className="block w-full shadow-2xl" 
-                      onClick={() => { window.open('./assest/kaddy9.jpg', '_self') }}
-                      />
-                    </SwiperSlide>
-                    <SwiperSlide>
-                        <img src="./assest/Carosal4.jpg"
-                            className="block w-full shadow-2xl" 
-                      onClick={() => { window.open('./assest/kaddy9.jpg', '_self') }}
+                      onClick={() => { window.open('./assest/Carosal3.jpg', '_self') }}
                       />
                     </SwiperSlide>
                 </Swiper>
