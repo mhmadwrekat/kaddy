a06d0ec054ba87fd2f92cd0cf3caafda49d9e648
mhmad wrekat <mhmmadwreekkat@gmail.com>
2022-05-04 17:25:48 +0300

    update

--- components/Nav.js
+++ components/Nav.js
@@ -5,7 +5,7 @@ const Nav = () => {
   return (
     <Head>
       <title>Kaddy Sales</title>
-      <link rel="icon" href="/assest/logo2.jpg" />
+      <link rel="icon" href="/assest/logo5.png" />
       <meta charset="UTF-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta http-equiv="X-UA-Compatible" content="ie=edge" />
